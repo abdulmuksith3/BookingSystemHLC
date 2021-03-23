cff8027ef440d2cb01e32bc69e3f2d650c63123e
Abdul Muksith Rizvi <57699415+abdulmuksith3@users.noreply.github.com>
2021-03-23 23:28:14 +0300

    ArrayList Added
    ArrayList for Student, Coach, Lessons
    Planning lessons timetable

--- src/bookingsystemhlc/BookingSystemHLC.java
+++ src/bookingsystemhlc/BookingSystemHLC.java
@@ -7,6 +7,7 @@ package bookingsystemhlc;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 /**
  *
  * @author abdul
@@ -27,50 +28,111 @@ public class BookingSystemHLC {
     }
     
     public static void generateData(){
-        generateCoachData();
-        generateStudentData();
-        generateLessonsData();
+        ArrayList<Coach> coachesAL = generateCoachData();
+        ArrayList <Student> studentsAL = generateStudentData();
+        ArrayList <Lessons> lessonsAL = generateLessonsData(coachesAL);
         generateBookingsData();
+        
+//        for (Coach c : coachesAL) {
+//            System.out.println(c.getId() + " " + c.getFullName());
+//        }
+//         for (Student c : studentsAL) {
+//            System.out.println(c.getId() + " " + c.getFullName());
+//        }
     }
     
-    public static void generateCoachData(){
-        //        Coach(String fullName, String address, String telephoneNo, String[] expertise, String officeHour)
+    public static ArrayList<Coach> generateCoachData(){
+        ArrayList <Coach> coachList = new ArrayList<Coach>();
         
+        //        Coach(String fullName, String address, String telephoneNo, String[] expertise, String officeHour)        
         //        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss"); 
         //        Coach rick = new Coach("Rick Grimes", "Hereford", "+44 7700 900140",null, LocalDateTime.of(2021,Month.MARCH,1,4,30).format(myFormatObj));
-
-        Coach rick = new Coach("Rick Grimes", "Hereford", "+44 7700 900110",new String[]{"Swimming", "Badminton"}, "Monday, 16:00-17:00");   
-        Coach glenn = new Coach("Glenn Rhee", "Wells", "+44 7700 900120",new String[]{"Gymnastics", "Tennis"}, "Tuesday, 16:00-17:00");
-        Coach negan = new Coach("Negan", "Southampton", "+44 7700 900130",new String[]{"Cricket", "Football"}, "Wednesday, 16:00-17:00");
-        Coach daryl = new Coach("Daryl Dixon", "Cambridge", "+44 7700 900140",new String[]{"Table Tennis", "Archery"}, "Thursday, 16:00-17:00");
-        Coach maggie = new Coach("Maggie Greene", "Worcester", "+44 7700 900150",new String[]{"Tennis", "Cricket"}, "Friday, 16:00-17:00");
-        Coach michonne = new Coach("Michonne", "Hereford", "+44 7700 900160",new String[]{"Swimming", "Gymnastics"}, "Friday, 17:00-18:00");
+                
+        Coach coach1 = new Coach("Rick Grimes", "Hereford", "+44 7700 900110",new String[]{"Swimming", "Badminton"}, "Monday, 16:00-17:00"); 
+        coachList.add(coach1);
+        
+        Coach coach2 = new Coach("Glenn Rhee", "Wells", "+44 7700 900120",new String[]{"Gymnastics", "Tennis"}, "Tuesday, 17:00-18:00");
+        coachList.add(coach2);
+        
+        Coach coach3 = new Coach("Negan", "Southampton", "+44 7700 900130",new String[]{"Cricket", "Football"}, "Wednesday, 17:00-18:00");
+        coachList.add(coach3);
+        
+        Coach coach4 = new Coach("Daryl Dixon", "Cambridge", "+44 7700 900140",new String[]{"Archery"}, "Thursday, 15:00-16:00");
+        coachList.add(coach4);
+        
+        Coach coach5 = new Coach("Maggie Greene", "Worcester", "+44 7700 900150",new String[]{"Tennis"}, "Friday, 18:00-19:00");
+        coachList.add(coach5);
+        
+        Coach coach6 = new Coach("Michonne", "Hereford", "+44 7700 900160",new String[]{"Swimming", "Gymnastics"}, "Friday, 17:00-18:00");
+        coachList.add(coach6);      
+        
+        
+        return coachList;
+        
+        
     }
     
-    public static void generateStudentData(){
+    public static ArrayList<Student> generateStudentData(){
+        ArrayList<Student> studentAL = new ArrayList<Student>();
+        
         //        Student(String fullName, String address, String telephoneNo
-        Student herby = new Student("Herby Stansell","3rd Parkway","+44 7700 900111");
-        Student sophi = new Student("Sophi Broadbear","Kedzie Junction","+44 7700 900220");
-        Student andres = new Student("Andres Ughini","Myrtle Terrace","+44 7700 900330");
-        Student sarette = new Student("Sarette Mintram","Sachs Way","+44 7700 900440");
-        Student trent = new Student("Trent Eilhermann","Loeprich Point","+44 7700 900550");
+        Student student1 = new Student("Herby Stansell","3rd Parkway","+44 7700 900111");
+        studentAL.add(student1);
         
-        Student janet = new Student("Janet McCosker","","+44 7700 900660");
-        Student mortie = new Student("Mortie Fenlon","Grover Way","+44 7700 900770");
-        Student maurice = new Student("Maurice Ritchley","Sachs Circle","+44 7700 900880");
-        Student rainer = new Student("Rainer Naile","Summer Ridge Road","+44 7700 900990");
-        Student lindon = new Student("Lindon Chessil","Nancy Center","+44 7700 900890");
+        Student student2 = new Student("Sophi Broadbear","Kedzie Junction","+44 7700 900220");
+        studentAL.add(student2);
         
-        Student reeta = new Student("Reeta Staite","Macpherson Junction","+44 7700 900790");
-        Student jenna = new Student("Jenna Trahearn","Canary Street","+44 7700 900590");
-        Student christine = new Student("Christine Jardine","Jenna Center","+44 7700 900450");
-        Student domeniga = new Student("Domeniga Guillon","Menomonie Point","+44 7700 900650");
-        Student leon = new Student("Leon Pettit","Ridgeview Point","+44 7700 900150");
+        Student student3 = new Student("Andres Ughini","Myrtle Terrace","+44 7700 900330");
+        studentAL.add(student3);
+        
+        Student student4 = new Student("Sarette Mintram","Sachs Way","+44 7700 900440");
+        studentAL.add(student4);
+        
+        Student student5 = new Student("Trent Eilhermann","Loeprich Point","+44 7700 900550");    
+        studentAL.add(student5);
+            
+        Student student6 = new Student("Janet McCosker","","+44 7700 900660");
+        studentAL.add(student6);
+        
+        Student student7 = new Student("Mortie Fenlon","Grover Way","+44 7700 900770");
+        studentAL.add(student7);
+        
+        Student student8 = new Student("Maurice Ritchley","Sachs Circle","+44 7700 900880");
+        studentAL.add(student8);
+        
+        Student student9 = new Student("Rainer Naile","Summer Ridge Road","+44 7700 900990");
+        studentAL.add(student9);
+        
+        Student student10 = new Student("Lindon Chessil","Nancy Center","+44 7700 900890");    
+        studentAL.add(student10);
+            
+        Student student11 = new Student("Reeta Staite","Macpherson Junction","+44 7700 900790");
+        studentAL.add(student11);
+        
+        Student student12 = new Student("Jenna Trahearn","Canary Street","+44 7700 900590");
+        studentAL.add(student12);
+        
+        Student student13 = new Student("Christine Jardine","Jenna Center","+44 7700 900450");
+        studentAL.add(student13);
+        
+        Student student14 = new Student("Domeniga Guillon","Menomonie Point","+44 7700 900650");
+        studentAL.add(student14);
+        
+        Student student15 = new Student("Leon Pettit","Ridgeview Point","+44 7700 900150");
+        studentAL.add(student15);
+        
+        return studentAL;
     }
     
-    public static void generateLessonsData(){
-        //        Lessons(String name, String place, String dateTime, Coach coach, int capacity)  
-        Lessons swimmingA = new Lessons("Swimming", "Swimming Pool", "Monday, 16:00-17:00", rick, 3);
+    public static ArrayList<Lessons> generateLessonsData(ArrayList<Coach> coachesAL){
+        
+        ArrayList<Lessons> lessonsAL = new ArrayList<Lessons>();
+        
+//        Lessons(String name, String place, String dateTime, Coach coach, int capacity)  
+
+        Lessons swimmingA = new Lessons("Swimming", "Swimming Pool", "Monday, 16:00-17:00", coachesAL.get(0), 3);
+        
+        return lessonsAL;
     }
     public static void generateBookingsData(){
         //        Bookings (Student student, Lessons lesson )
